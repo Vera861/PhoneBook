5a83deb7ec49ab70aed50d8148a6399343de1210
Vera <mverynchik@gmail.com>
2021-04-12 17:45:49 +0300

    create List

--- PhoneBook/src/ru/geekbrains/PhoneBook/Main.java
+++ PhoneBook/src/ru/geekbrains/PhoneBook/Main.java
@@ -1,8 +1,21 @@
 package ru.geekbrains.PhoneBook;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> surnames = List.of("Морозов", "Радулов", "Панарин", "Ковальчук", "Морозов", "Мозякин",
+                "Дацюк", "Сорокин", "Кошечкин", "Радулов", "Капризов", "Кузнецов","Морозов");
+        Map<String, Integer> duplex = new HashMap<>();
+        Integer b = null;
+        for (String s: surnames) {
+            duplex.compute(s, b+1);
+        }
+
+
     }
 }
