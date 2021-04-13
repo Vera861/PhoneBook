68acc4987b30bf2a80ee17e7f42f927bb91cfc7d
Vera <mverynchik@gmail.com>
2021-04-13 04:07:07 +0300

    full version
    Signed-off-by: Vera <mverynchik@gmail.com>

---
+++ PhoneBook/src/ru/geekbrains/PhoneBook/Main.java
@@ -0,0 +1,34 @@
+package ru.geekbrains.PhoneBook;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Phonebook pb = new Phonebook();
+        pb.add("Радулов", "+7(928)632-14-52");
+        pb.add("Радулов", "+7(962)442-14-52");
+        pb.add("Сорокин", "+7(988)638-98-89");
+        System.out.println(pb.get("Радулов"));
+        System.out.println(pb.get("Сорокин"));
+        System.out.println(pb.get("Мозякин"));
+//        chageList();
+    }
+
+    public static void chageList() {
+        List<String> surnames = new ArrayList<>(List.of("Морозов", "Радулов", "Панарин", "Ковальчук",
+                "Морозов", "Мозякин", "Дацюк", "Морозов", "Сорокин", "Кошечкин", "Радулов", "Мозякин", "Капризов",
+                "Кузнецов", "Морозов"));
+        Map<String, Integer> duplex = new HashMap<>();
+        for (int i = 0; i < surnames.size(); i++) {
+            String s = surnames.get(i);
+            Integer repName = duplex.get(s);
+            duplex.put(s, repName == null ? 1 : repName + 1);
+        }
+        System.out.println(duplex);
+    }
+}
